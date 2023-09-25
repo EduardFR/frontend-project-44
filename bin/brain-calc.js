c718c9edec24f3c111f074ea60f99de7cdeaf82b
EduardFR <118263500+EduardFR@users.noreply.github.com>
2023-09-25 20:49:24 +0500

    added game Calculator

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import launcherGame from '../src/games/calculator.js';
+
+launcherGame('What is the result of the expression?');
---
+++ src/games/calculator.js
@@ -0,0 +1,30 @@
+import launcherGame from '../index.js';
+
+const calculatedAnswer = (number1, number2, operator) => {
+  if (operator === '-') {
+    return number1 - number2;
+  }
+  if (operator === '+') {
+    return number1 + number2;
+  }
+  if (operator === '*') {
+    return number1 * number2;
+  }
+};
+
+const calculator = () => {
+  const firstNumber = Math.floor(Math.random() * 101);
+  const secondNumber = Math.floor(Math.random() * 101);
+
+  const operators = ['-', '+', '*'];
+  const operatorIndex = Math.floor(Math.random() * 3);
+
+  const operator = operators[operatorIndex];
+
+  const rightAnswer = calculatedAnswer(firstNumber, secondNumber, operator);
+  const question = `Question: ${firstNumber} ${operator} ${secondNumber}`;
+
+  return [question, rightAnswer.toString()];
+};
+
+export default (description) => launcherGame(calculator, description);
